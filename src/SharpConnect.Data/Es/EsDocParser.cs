1d9c6731e0ae335e4f9eba74a0bdb4ca063374fc
enginekit <enginecollection@gmail.com>
2016-08-13 11:47:02 +0700

    28: new EaseDocParser

---
+++ src/SharpConnect.Data/Es/EsDocParser.cs
@@ -0,0 +1,36 @@
+﻿//MIT, 2015-2016, brezza92, EngineKit and contributors
+
+using System;
+
+namespace SharpConnect.Data
+{
+    class EaseDocParser : Es.EsParserBase<EsElem, EsArr>
+    {
+        EaseDocument easeDoc;
+        public EaseDocParser(EaseDocument blankdoc)
+        {
+            easeDoc = blankdoc;
+        }
+        protected override EsElem createElement()
+        {
+            return easeDoc.CreateElement();
+        }
+
+        protected override EsArr createArray()
+        {
+            return easeDoc.CreateArray();
+        }
+
+        protected override void addElementAttribute(EsElem targetElem, string key, object value)
+        {
+            targetElem[key] = value;
+        }
+
+        protected override void addArrayElement(EsArr targetArray, object value)
+        {
+            targetArray.AddItem(value);
+        } 
+    }
+
+}
+
